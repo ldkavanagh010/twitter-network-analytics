c6380066c91935e0eec7295f91a7a8de25cb249d
Ubuntu <ubuntu@ip-10-0-0-10.ec2.internal>
2020-02-09 17:57:59 +0000

    code cleaning and algorithm tweaks

--- ingestion/html_parser.py
+++ ingestion/html_parser.py
@@ -11,22 +11,29 @@ class MyParser(HTMLParser):
 			for name, value in attrs:
 				# If href is defined, print it.
 				if name == "href" and ((value[-3:] == 'zst') or (value[-3:] == 'tar')):
-					self.hrefs.append(value)
+					if value[-3:] == 'zst':
+						self.hrefs.append((value, 'zst'))
+					if value[-3:] == 'tar':
+						self.hrefs.append((value, 'tar'))
+
 
 
 class HTMLHandler():
 
-	def __init__(self, website):
+	def __init__(self):
 		self.http = urllib3.PoolManager()
-		self.website = website
-		self.links = []
 
-	def _get_html(self):
-		return self.http.request('GET', self.website).data.decode('utf-8')
+	def _get_html(self, website):
+		return self.http.request('GET', website).data.decode('utf-8')
 
-	def get_links(self):
+	def get_links(self, website):
+		links = []
 		parser = MyParser()
-		parser.feed(self._get_html())
+		parser.hrefs = list()
+		parser.feed(self._get_html(website))
 		for href in parser.hrefs:
-			self.links.append(href.replace(".", self.website, 1))
-		return self.links
+			if href[1] == 'zst':
+				links.append(href[0].replace(".", website, 1))
+			else:
+				links.append(website + href[0])
+		return links
